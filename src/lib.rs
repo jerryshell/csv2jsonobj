6e5ffe51790713dd7e3e6bdcac545b282a4f1424
jerry <calljerryli@outlook.com>
2024-12-22 05:15:17 +0800

    Refactor csv_to_json function for improved readability and performance
    - Updated the csv_to_json function to use iterator methods for processing records.
    - Simplified the creation of JSON objects by using fold with headers.
    - Changed return type to explicitly use serde_json::Value.
    - Enhanced string_to_json_value function to handle boolean values more clearly.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,35 @@
-use serde_json::{json, Value};
-use std::error::Error;
-
-pub fn csv_to_json(csv_path: &str, key_index: usize) -> Result<Value, Box<dyn Error>> {
+pub fn csv_to_json(
+    csv_path: &str,
+    key_index: usize,
+) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
     let mut reader = csv::Reader::from_path(csv_path)?;
-
     let headers = reader.headers()?.clone();
-    let mut result = json!({});
 
-    for record in reader.records().flatten() {
-        let record_json = record
-            .iter()
-            .enumerate()
-            .fold(json!({}), |mut json, (i, field)| {
-                let key = headers.get(i).unwrap_or("UNKNOWN_HEADER");
-                let value = string_to_json_value(field);
-                json[key] = value;
-                json
-            });
-        result[record[key_index].to_string()] = record_json;
-    }
+    let result = reader
+        .records()
+        .filter_map(Result::ok)
+        .map(|record| {
+            let record_json = headers.iter().zip(record.iter()).fold(
+                serde_json::json!({}),
+                |mut acc, (header, field)| {
+                    acc[header] = string_to_json_value(field);
+                    acc
+                },
+            );
+            (record[key_index].to_string(), record_json)
+        })
+        .collect::<serde_json::Map<String, serde_json::Value>>();
 
-    Ok(result)
+    Ok(serde_json::Value::Object(result))
 }
 
-fn string_to_json_value(string: &str) -> Value {
+fn string_to_json_value(string: &str) -> serde_json::Value {
     match string.parse::<f64>() {
-        Ok(f) => json!(f),
-        Err(_) => match string {
-            "TRUE" => json!(true),
-            "FALSE" => json!(false),
-            _ => json!(string),
+        Ok(f) => serde_json::json!(f),
+        Err(_) => match string.to_uppercase().as_str() {
+            "TRUE" => serde_json::json!(true),
+            "FALSE" => serde_json::json!(false),
+            _ => serde_json::json!(string),
         },
     }
 }
